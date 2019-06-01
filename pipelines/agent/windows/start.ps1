9fbcbff1228634c970177b30d303dd863d5e55f9
Steven Follis <steven.follis@docker.com>
2019-06-01 07:37:21 +0200

    Updated to use ServiceMonitor

--- pipelines/agent/windows/start.ps1
+++ pipelines/agent/windows/start.ps1
@@ -1,3 +1,5 @@
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/agents/v2-windows
+
 # Check if required variables are present
 if (!$env:AZP_URL) { Write-Host "The AZP_URL environment variable is null. Please adjust before continuing"; exit 1; }
 if (!$env:AZP_TOKEN) { Write-Host "The AZP_TOKEN environment variable is null. Please adjust before continuing"; exit 1; }
@@ -6,16 +8,24 @@ if (!$env:AZP_POOL) { $env:AZP_POOL='Default' }
 # ===============================
 # Configure Azure Pipelines Agent
 # ===============================
-.\config.cmd `
-  --unattended `
-  --url "${env:AZP_URL}" `
-  --auth PAT `
-  --token "${env:AZP_TOKEN}" `
-  --pool "${env:AZP_POOL}" `
-  --replace `
-  --acceptTeeEula
+if(!(Test-Path -Path C:\agent\_work )) {
+  
+  Write-Output "No previous agent configuration detected. Configuring agent."
+  
+  .\config.cmd `
+    --acceptTeeEula `
+    --auth PAT `
+    --pool "${env:AZP_POOL}" `
+    --replace `
+    --runAsService `
+    --token "${env:AZP_TOKEN}" `
+    --unattended `
+    --url "${env:AZP_URL}" `
+    --windowsLogonAccount "NT AUTHORITY\SYSTEM"
+
+}
 
 # ==============================
-# Run Azure Pipelines Agent
+# Run Azure Pipelines Agent with ServiceMonitor
 # ==============================
-.\run.cmd
+C:\ServiceMonitor.exe (Get-Service vstsagent*).Name
